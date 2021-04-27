c4a2c03e938d7439d6d4fd1acc8b628d110e57fb
moonormal <a334259@uach.mx>
2021-04-27 00:55:38 -0600

    22.-  Introducir  una  frase  por  teclado.  Imprimirla  cinco  veces  en  filas  consecutivas,  pero  cada impresión ir desplazada cuatro columnas hacia la derecha.

---
+++ 22.js
@@ -0,0 +1,17 @@
+var readline = require('readline'); 
+
+var rl = readline.createInterface({ 
+    input: process.stdin, 
+    output: process.stdout 
+
+}); 
+
+
+rl.question("Ingrese una frase ", function(answer) { 
+    console.log(`   ${answer}`); 
+    console.log(`       ${answer}`);
+    console.log(`           ${answer}`);
+    console.log(`               ${answer}`);
+    console.log(`                   ${answer}`);
+    rl.close(); 
+});
